d7ff80d55a8956853d31ea3fed58945871ac800e
cbarringer <cbarringer@mjm.com>
2022-12-29 13:37:22 -0600

    This is my initial checkin of the Main class

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import com.google.gson.Gson;
+
+/**
+ * This is my startup class
+ */
+public class Main {
+    /**
+     * This is the startup method
+     * @param args This is the collection of strings passed from the command line.
+     *              By Genes
+     */
+    public static void main(String[] args) {
+        Gson mygson = new Gson();
+        System.out.println("Hello from my jar file");
+
+    }
+}
