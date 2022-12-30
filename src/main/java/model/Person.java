10510b5f3c276850c52042f7e390636eef9927e2
cbarringer <cbarringer@mjm.com>
2022-12-30 16:05:24 -0600

    updated the person class, added title to it

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -4,11 +4,13 @@ public class Person {
     private String firstName;
     private String lastName;
     private int age;
+    private String title;
 
-    public Person(String firstName, String lastName, int age) {
+    public Person(String firstName, String lastName, int age, String title) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+        this.title = title;
     }
 
     public String getFirstName() {
@@ -31,16 +33,25 @@ public class Person {
         return age;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     @Override
     public String toString() {
         return "Person{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", age=" + age +
+                ", title='" + title + '\'' +
                 '}';
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 }
